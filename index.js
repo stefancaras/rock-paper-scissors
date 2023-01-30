ac071ba6ecea4f5e06b01c91564f2b82674af44b
Stefan Caras <stefan.caras@gmail.com>
2023-01-30 21:39:25 +0200

    improvements

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const $ = query => document.querySelector(query)
 const array = [["r", "p", "s"], ["p", "r"], [0, 0]]
+const msg = { r: "Rock crushes scissors.", p: "Paper covers rock.", s: "Scissors cut paper." }
 
 $(".main-div").addEventListener("click", (e) => {
   if (e.target.id) {
@@ -9,12 +10,17 @@ $(".main-div").addEventListener("click", (e) => {
     $(".img-comp").innerHTML = `<img src="./img/${array[1][1]}.png" />`
     const choices = array[1].join("")
     if (choices === "rs" || choices === "sp" || choices === "pr") {
-      $(".message").textContent = `You won`
+      $(".message").innerHTML = `<p class="greenBg">You won</p>`
+      $(".message2").textContent = msg[array[1][0]]
       array[2][0]++
     } else if (choices === "sr" || choices === "ps" || choices === "rp") {
-      $(".message").textContent = `The computer won`
+      $(".message").innerHTML = `<p class="redBg">The computer won</p>`
+      $(".message2").textContent = msg[array[1][1]]
       array[2][1]++
-    } else $(".message").textContent = `It's a tie, try again`
+    } else {
+      $(".message").innerHTML = `<p class="yellowBg">It's a tie, try again</p>`
+      $(".message2").textContent = ""
+    }
     $("#user-score").textContent = array[2][0]
     $("#comp-score").textContent = array[2][1]
   }
