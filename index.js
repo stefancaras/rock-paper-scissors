61969f8dfbdec99b77511fc5e0c33988a09c163e
Stefan Caras <stefan.caras@gmail.com>
2023-01-30 19:34:45 +0200

    clean code

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-const mainDiv = document.querySelector(".main-div")
-const imgUser = document.querySelector(".img-user")
-const imgComp = document.querySelector(".img-comp")
-const message = document.querySelector(".message")
-const userScoreSpan = document.querySelector("#user-score")
-const compScoreSpan = document.querySelector("#comp-score")
+const $ = query => document.querySelector(query)
+const array = [["r", "p", "s"], ["p", "r"], [0, 0]]
 
-const array = ["r", "p", "s"]
-let userChoice, compChoice
-let userScore = 0
-let compScore = 0
-
-message.textContent = `Choose:`
-imgUser.innerHTML = `<img src="./img/${array[Math.floor(Math.random() * 3)]}.png" />`
-
-const randomImg = () => {
-  const randomNumber = Math.floor(Math.random() * 3)
-  compChoice = array[randomNumber]
-  imgComp.innerHTML = `<img src="./img/${compChoice}.png" />`
-}
-randomImg()
-
-const logic = () => {
-  if ((userChoice === "r" && compChoice === "s") ||
-    (userChoice === "s" && compChoice === "p") ||
-    (userChoice === "p" && compChoice === "r")) {
-    message.textContent = `You won`
-    userScore++
-  } else if ((userChoice === "s" && compChoice === "r") ||
-    (userChoice === "p" && compChoice === "s") ||
-    (userChoice === "r" && compChoice === "p")) {
-    message.textContent = `The computer won`
-    compScore++
-  } else if (userChoice === compChoice) {
-    message.textContent = `It's a tie, try again`
-  }
-  userScoreSpan.textContent = userScore
-  compScoreSpan.textContent = compScore
-}
-
-mainDiv.addEventListener("click", (e) => {
+$(".main-div").addEventListener("click", (e) => {
   if (e.target.id) {
-    userChoice = e.target.id
-    imgUser.innerHTML = `<img src="./img/${userChoice}.png" />`
-    randomImg()
-    logic()
+    array[1][0] = e.target.id
+    array[1][1] = array[0][Math.floor(Math.random() * 3)]
+    $(".img-user").innerHTML = `<img src="./img/${array[1][0]}.png" />`
+    $(".img-comp").innerHTML = `<img src="./img/${array[1][1]}.png" />`
+    const choices = array[1].join("")
+    if (choices === "rs" || choices === "sp" || choices === "pr") {
+      $(".message").textContent = `You won`
+      array[2][0]++
+    } else if (choices === "sr" || choices === "ps" || choices === "rp") {
+      $(".message").textContent = `The computer won`
+      array[2][1]++
+    } else $(".message").textContent = `It's a tie, try again`
+    $("#user-score").textContent = array[2][0]
+    $("#comp-score").textContent = array[2][1]
   }
 })
